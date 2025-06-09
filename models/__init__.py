8e8c793a9a8bd7441e76aa2800d85871db2f37b9
angelreyes117 <angelreyes.117@gmail.com>
2025-06-09 00:38:17 +0000

    webflask

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Initialize the storage engine depending on HBNB_TYPE_STORAGE
+"""
+
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+storage.reload()
