98e0687bf215b84d02c39d8a98add53d599964f7
angelreyes117 <angelreyes.117@gmail.com>
2025-06-13 04:42:06 +0000

    airbnb clone

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
-"""
- Test cities access from a state
+#!/usr/bin/python3                                                                                                                                                                                                                        
+"""                                                                                                                                                                                                                                       
+ Test cities access from a state                                                                                                                                                                                                          
 """
 from models import storage
 from models.state import State
 from models.city import City
 
-"""
- Objects creations
+"""                                                                                                                                                                                                                                       
+ Objects creations                                                                                                                                                                                                                        
 """
 state_1 = State(name="California")
 print("New state: {}".format(state_1))
@@ -27,8 +27,8 @@ print("New city: {} in the state: {}".format(city_2_1, state_2))
 city_2_1.save()
 
 
-"""
- Verification
+"""                                                                                                                                                                                                                                       
+ Verification                                                                                                                                                                                                                             
 """
 print("")
 all_states = storage.all(State)
