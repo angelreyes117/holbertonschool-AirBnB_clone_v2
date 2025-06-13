8bf8f5ece1487c00b69b3b72aa5b290cfdb03ce7
angelreyes117 <angelreyes.117@gmail.com>
2025-06-13 07:02:58 +0000

    airbnb clone

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+"""Starts a Flask web application that lists States"""
 from flask import Flask, render_template
-from models import *
 from models import storage
+
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage on teardown"""
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays a HTML page with the list of States"""
+    states = storage.all("State")
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
