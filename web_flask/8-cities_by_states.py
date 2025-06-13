7ad274fa3539123f9b319ed0eee810a9815d2254
angelreyes117 <angelreyes.117@gmail.com>
2025-06-13 07:17:33 +0000

    aribnb clone

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,13 +10,21 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     """
-    /states_list: display a HTML page: (inside the tag BODY)
+    Routes:
+    /cities_by_states: display a HTML page: (inside the tag BODY)
+    H1 tag: “States”
+    UL tag: with the list of all State objects present
+    in DBStorage sorted by name (A->Z) tip
+    LI tag: description of one State: <state.id>:
+    <B><state.name></B> + UL tag: with the list of City objects
+    linked to the State sorted by name (A->Z)
+    LI tag: description of one City: <city.id>: <B><city.name></B>
     """
     states = storage.all('State')
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
