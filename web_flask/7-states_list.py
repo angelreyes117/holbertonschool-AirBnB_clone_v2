81b332b70d7c9b7a3d366363a2cff374b4412653
angelreyes117 <angelreyes.117@gmail.com>
2025-06-13 07:49:20 +0000

    airbnb clone

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""pt that runs a Flask web application"""
-
+""" Flask app that lists states """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
-
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(exception):
+    """Remove current SQLAlchemy Session"""
     storage.close()
 
-
 @app.route('/states_list', strict_slashes=False)
-def states():
-    """List all states"""
-    states = storage.all("State")
-    # states_dic = {state['id']: state['name'] for state in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States", states=states)
+def states_list():
+    """Display HTML page with list of states"""
+    states = storage.all(State)
+    states_sorted = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states_sorted)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
