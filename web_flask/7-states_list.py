ddf4e8bfd96696ab24cea413478261042c2fd1ea
angelreyes117 <angelreyes.117@gmail.com>
2025-06-13 08:17:41 +0000

    airbnb clone

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+"""
+flask model
+"""
 from flask import Flask, render_template
-import models
-
+from models import storage
+from models.state import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(content):
-    """ removes current SQLAlchemy sesh """
-    models.storage.close()
+def teardown_data(self):
+    """
+        refrech data
+    """
+    storage.close()
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ prints list of states """
-    states = models.storage.all(models.state.State)
+    """ return all states in the db  """
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
