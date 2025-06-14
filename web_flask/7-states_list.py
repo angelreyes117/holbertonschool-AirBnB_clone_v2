80e8a283851d972ccd4be8d777341075bea86666
Ángel Daniel González Reyes <149020964+angelreyes117@users.noreply.github.com>
2025-06-13 23:59:03 -0600

    airbnb clone

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+You must use storage for fetching data from
+the storage engine (FileStorage or DBStorage) =>
+from models import storage and storage.all(...)
 """
-
-from flask import Flask, render_template
-from models import *
 from models import storage
+from flask import render_template, Flask
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    """
+    /states_list: display a HTML page: (inside the tag BODY)
+    """
+    states = storage.all('State')
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exc):
+    """
+    After each request you must remove the current SQLAlchemy Session:
+    Declare a method to handle @app.teardown_appcontext
+    Call in this method storage.close()
+    """
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
