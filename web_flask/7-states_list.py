7c874cb86a1a9ef75838725c605f2c2e4f268853
angelreyes117 <angelreyes.117@gmail.com>
2025-06-26 09:54:17 +0000

    airbnb clone v2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 ''' Flask web application '''
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models.state import State
 from models import storage
 
 app = Flask(__name__)
 
-
 @app.route('/states_list', strict_slashes=False)
 def state_list():
-    all_states = storage.all(State)
+    # Get all State objects as a dictionary, convert to list, and sort by name
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda state: state.name)
     return render_template('7-states_list.html', states=all_states)
-
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
+    # Close the storage session after each request
     return storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
