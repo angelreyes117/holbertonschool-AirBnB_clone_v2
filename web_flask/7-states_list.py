3b52edd0a88223d01b9aa89b338fc4869763b8d6
angelreyes117 <angelreyes.117@gmail.com>
2025-06-27 07:26:44 +0000

    airbnb clone v2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""Starts a Flask web application for /states_list"""
+"""
+Flask web app for /states_list route
+"""
 from flask import Flask, render_template
-from models import storage  # Importar storage correctamente
+from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Render HTML page with H1 tag 'States' and list of State objects sorted by name."""
-    # Obtener todos los objetos State y ordenarlos por nombre
-    states = sorted(storage.all("State").values(), key=lambda st: st.name)
-    # Pasar la lista de estados a la plantilla
-    return render_template('7-states_list.html', states=states)
-
+    """Display HTML page with list of all State objects sorted by name"""
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Close storage session after each request."""
+    """Remove SQLAlchemy session"""
     storage.close()
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
