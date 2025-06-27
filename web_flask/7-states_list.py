9386bf401af86a1d9055580ac6f206eea08c688a
angelreyes117 <angelreyes.117@gmail.com>
2025-06-27 08:45:15 +0000

    airbnb clone v2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,12 +5,11 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
-
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
@@ -18,15 +17,13 @@ def states_list():
 
     States are sorted by name.
     """
-    states = storage.all("State")
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
-
 
 @app.teardown_appcontext
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
